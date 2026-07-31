fdf29c9331d2a79b43092f9c35fdfa09b6aabc21
AI-047 <25cseaiml047.subhamkumarrout@giet.edu>
2026-07-31 08:33:23 +0530

    Added DFS Graph Traversal Program

---
+++ DFS.py
@@ -0,0 +1,41 @@
+def dfs(graph,start_node):
+    visited = []
+    stack =[start_node]
+    
+    while stack:
+        current_node = stack.pop()
+        
+        if current_node not in visited:
+            print(f"Exploring node1: {current_node}")
+            visited.append(current_node)
+
+            for neighbour in graph.get(current_node,[]):
+                if neighbour not in visited and neighbour not in stack:
+                    stack.append(neighbour)
+    
+    return visited
+
+print("---Build Your Graph---")
+student_graph = {}
+
+num_edges = int(input("How many edges (connection) does your graph has ?"))
+
+print("Enter each edge separated by a spacce(e.g., A B):")
+for i in range(num_edges):
+    u,v = input(f"Edge {i+1}").split()
+    
+    if u not in student_graph:
+        student_graph[u] = []
+    if v not in student_graph:
+        student_graph[v] = []
+        
+    student_graph[u].append(v)
+    student_graph[v].append(u)
+    
+start = input("Enter the starting node for DFS: ")    
+
+print(f"\nYour Graph Dictionary: {student_graph}")
+print("Staring DFS Traversal...")
+
+result = dfs(student_graph,start)
+print(f"DFS traversal order: {result}")
